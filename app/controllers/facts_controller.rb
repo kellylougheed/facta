2b7e64882ce88c166c92b029c1c39dedffafafcf
Kelly Lougheed <kellylougheed@gmail.com>
2016-10-28 22:54:21 +0000

    Add new fact modal

--- app/controllers/facts_controller.rb
+++ app/controllers/facts_controller.rb
@@ -2,4 +2,19 @@ class FactsController < ApplicationController
   def index
     @fact = Fact.order("RANDOM()").first
   end
+
+  def new
+  end
+
+  def create
+    Fact.create(fact_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def fact_params
+    params.require(:fact).permit(:text, :initial_citation)
+  end
+
 end
